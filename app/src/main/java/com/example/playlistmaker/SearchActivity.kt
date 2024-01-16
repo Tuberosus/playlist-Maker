3f5cae34419ad143db4f44f864a540f971421cd2
ber9z <berez.a@bk.ru>
2024-01-16 20:35:28 +0300

    Sprint 9 HW#4

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -11,12 +11,22 @@ import android.widget.EditText
 import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+
+    private var inputTextValue = DEF_TEXT
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        val backButton = findViewById<ImageView>(R.id.imageBackAction)
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        if (savedInstanceState != null) inputEditText.setText(inputTextValue)
+
+        backButton.setOnClickListener {
+            finish()
+        }
 
         clearButton.setOnClickListener {
             clearButton.visibility = View.GONE
@@ -31,7 +41,8 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (!p0.isNullOrEmpty()) clearButton.visibility = clearButtonVisibility(p0)
+                clearButton.visibility = clearButtonVisibility(p0)
+                inputTextValue = p0.toString()
             }
 
             override fun afterTextChanged(p0: Editable?) {
@@ -41,11 +52,26 @@ class SearchActivity : AppCompatActivity() {
         inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT, inputTextValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputTextValue = savedInstanceState.getString(INPUT_TEXT, DEF_TEXT)
+    }
+
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
         } else {
             View.VISIBLE
         }
+    }
+
+    companion object {
+        const val INPUT_TEXT = "INPUT_TEXT"
+        const val DEF_TEXT = ""
     }
 }
