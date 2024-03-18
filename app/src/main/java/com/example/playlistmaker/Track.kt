4a40c053915f0fbc3c955489d0d9c39685ac0519
ber9z <berez.a@bk.ru>
2024-03-18 15:59:47 +0300

    Sprint 13 HW#1

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,12 +1,22 @@
 package com.example.playlistmaker
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 class Track (
     val trackId: Int,
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val trackTimeMillis: String, // Продолжительность трека
-    val artworkUrl100: String // Ссылка на изображение обложки
+    val artworkUrl100: String, // Ссылка на изображение обложки
+    val collectionName: String?, // Название альбома
+    val releaseDate: String, // Год релиза трека
+    val primaryGenreName: String, // Жанр трека
+    val country: String // Страна исполнителя
 ) {
+    val trackTime get() = SimpleDateFormat("mm:ss", Locale.getDefault())
+        .format(trackTimeMillis.toInt())
+    val artworkUrl512 get() = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 
 class TrackAdapter() : RecyclerView.Adapter<TrackViewHolder> () {
 
@@ -25,6 +27,22 @@ class TrackAdapter() : RecyclerView.Adapter<TrackViewHolder> () {
                 addTrackToHistory(trackList[position])
                 write(trackSearchHistory)
             }
+
+            //переход в аудиоплеер
+            val displayIntent = Intent(holder.itemView.context, AudioPlayerActivity::class.java)
+            displayIntent.apply {
+                putExtra("trackName", trackList[position].trackName)
+                putExtra("artistName", trackList[position].artistName)
+                putExtra("artworkUrl100", trackList[position].artworkUrl100)
+                putExtra("trackTimeMillis", trackList[position].trackTimeMillis)
+                putExtra("collectionName", trackList[position].collectionName)
+                putExtra("releaseDate", trackList[position].releaseDate)
+                putExtra("primaryGenreName", trackList[position].primaryGenreName)
+                putExtra("country", trackList[position].country)
+            }
+            val json = Gson().toJson(trackList[position])
+            displayIntent.putExtra("track",json)
+            holder.itemView.context.startActivity(displayIntent)
         }
     }
 }
