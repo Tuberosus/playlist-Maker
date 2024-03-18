55cdfee62e643b606b8415109c442bbab83f7edc
ber9z <berez.a@bk.ru>
2024-03-18 16:15:47 +0300

    Sprint 13 HW#2

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -30,16 +30,6 @@ class TrackAdapter() : RecyclerView.Adapter<TrackViewHolder> () {
 
             //переход в аудиоплеер
             val displayIntent = Intent(holder.itemView.context, AudioPlayerActivity::class.java)
-            displayIntent.apply {
-                putExtra("trackName", trackList[position].trackName)
-                putExtra("artistName", trackList[position].artistName)
-                putExtra("artworkUrl100", trackList[position].artworkUrl100)
-                putExtra("trackTimeMillis", trackList[position].trackTimeMillis)
-                putExtra("collectionName", trackList[position].collectionName)
-                putExtra("releaseDate", trackList[position].releaseDate)
-                putExtra("primaryGenreName", trackList[position].primaryGenreName)
-                putExtra("country", trackList[position].country)
-            }
             val json = Gson().toJson(trackList[position])
             displayIntent.putExtra("track",json)
             holder.itemView.context.startActivity(displayIntent)
